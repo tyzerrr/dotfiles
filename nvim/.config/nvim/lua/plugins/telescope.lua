79d08127623af8b38f8df6597202b7fa329db5d5
Taichi Araki <araki130710@gmail.com>
2025-07-08 13:03:55 +0900

    feat: make telescope can find even for hidden files

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -3,6 +3,13 @@ return {
     tag = '0.1.8', -- 特定のバージョンを使う場合
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
+        require('telescope').setup({
+            pickers = {
+                find_files = {
+                    hidden = true,
+                },
+            },
+        })
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
