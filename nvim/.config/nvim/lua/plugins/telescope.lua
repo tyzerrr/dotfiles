5ddd5ee729c941847c418863b01980bc60bfa31d
Taichi Araki <araki130710@gmail.com>
2025-07-10 21:05:37 +0900

    feat: change telescope config

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,20 +1,35 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8', -- 特定のバージョンを使う場合
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-        require('telescope').setup({
-            pickers = {
-                find_files = {
-                    hidden = true,
-                },
-            },
-        })
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
-        vim.keymap.set('n', 'gr', builtin.lsp_references, {})
-        vim.keymap.set('n', 'gd', builtin.lsp_definitions, {})
-    end,
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.8",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		require("telescope").setup({
+			pickers = {
+				find_files = {
+					hidden = true,
+				},
+			},
+			defaults = {
+				file_ignore_patterns = {
+					"node_modules",
+					"%.git",
+					"%.DS_Store",
+					"%.zip",
+					"%.bin",
+					"tar",
+					"jar",
+					"data",
+					"doc",
+					"LICENCE",
+					"manifest",
+				},
+			},
+		})
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+		vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+		vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
+		vim.keymap.set("n", "gr", builtin.lsp_references, {})
+		vim.keymap.set("n", "gd", builtin.lsp_definitions, {})
+	end,
 }
