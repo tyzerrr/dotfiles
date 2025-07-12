ef3fe3b3084202eb8a21ee0321cd334889c4f0c8
Taichi Araki <araki130710@gmail.com>
2025-07-12 20:45:56 +0900

    feat: add refactoring.nvim remap

--- nvim/.config/nvim/lua/plugins/refactoring.lua
+++ nvim/.config/nvim/lua/plugins/refactoring.lua
@@ -6,4 +6,18 @@ return {
 	},
 	lazy = false,
 	opts = {},
+	config = function()
+		local refactoring = require("refactoring")
+		refactoring.setup({
+			vim.keymap.set("n", "<leader>rp", function()
+				refactoring.debug.printf({ below = false })
+			end),
+			vim.keymap.set({ "n", "x" }, "<leader>rv", function()
+				refactoring.debug.print_var()
+			end),
+			vim.keymap.set({ "n", "x" }, "<leader>rc", function()
+				refactoring.debug.cleanup()
+			end),
+		})
+	end,
 }
