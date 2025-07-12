652cfbfb16c92a47997c89584f54b54b1a5ccc96
Taichi Araki <araki130710@gmail.com>
2025-07-12 23:30:31 +0900

    feat: add oil.nvim, awesome

--- nvim/.config/nvim/lua/core/remap.lua
+++ nvim/.config/nvim/lua/core/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 --auto indenting and move multiple lines.
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
