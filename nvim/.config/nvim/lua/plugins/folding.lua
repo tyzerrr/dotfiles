da8a66078b805154d8c355f14a74586cefad8535
Taichi Araki <araki130710@gmail.com>
2025-07-12 18:34:22 +0900

    feat: add folding and make looks better

--- nvim/.config/nvim/lua/plugins/folding.lua
+++ nvim/.config/nvim/lua/plugins/folding.lua
@@ -1,7 +1,27 @@
 return {
 	"kevinhwang91/nvim-ufo",
-	dependencies = { "kevinhwang91/promise-async" },
+	event = "VeryLazy",
+	dependencies = {
+		"kevinhwang91/promise-async",
+	},
 	config = function()
-		require("ufo").setup()
+		vim.o.foldlevel = 99
+		vim.o.foldlevelstart = 99
+		vim.o.foldenable = true
+		vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+		vim.keymap.set("n", "zp", require("ufo").peekFoldedLinesUnderCursor)
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "neo-tree" },
+			callback = function()
+				require("ufo").detach()
+				vim.opt_local.foldenable = false
+			end,
+		})
+		require("ufo").setup({
+			provider_selector = function(_, _, _)
+				return { "treesitter", "indent" }
+			end,
+		})
 	end,
 }
