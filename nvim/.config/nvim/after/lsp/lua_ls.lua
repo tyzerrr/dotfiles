da6153bd859297b3b7ed8f8c3d15e270a602344d
Taichi Araki <araki130710@gmail.com>
2025-07-12 16:02:38 +0900

    feat: enable vim.api completion

--- nvim/.config/nvim/after/lsp/lua_ls.lua
+++ nvim/.config/nvim/after/lsp/lua_ls.lua
@@ -4,8 +4,15 @@ return {
 			runtime = {
 				version = "LuaJIT",
 			},
-			diagnostics = {
-				globals = { "vim" },
+			workspace = {
+				checkThirdParty = false,
+				library = {
+					"${3rd}/luv/library",
+					unpack(vim.api.nvim_get_runtime_file("", true)),
+				},
+			},
+			completion = {
+				callSnippet = "Replace",
 			},
 		},
 	},
