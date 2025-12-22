164dc9fe19a295dd067d7afbbad40ec9c5b6afbb
Taichi Araki <araki130710@gmail.com>
2025-12-22 23:45:52 +0900

    feat: add aqua configuration

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -36,55 +36,13 @@ return {
     -- 		ColorMyPencils()
     -- 	end,
     -- },
-
     -- {
-    --     "folke/tokyonight.nvim",
-    --     priority = 1000,
+    --     "rebelot/kanagawa.nvim",
+    --     lazy = false,    -- 起動時にロード
+    --     priority = 1000, -- 他のUIプラグインより先にロード
     --     config = function()
-    --         local transparent = false -- set to true if you would like to enable transparency
-    --
-    --         local bg = "#011628"
-    --         local bg_dark = "#011423"
-    --         local bg_highlight = "#143652"
-    --         local bg_search = "#0A64AC"
-    --         local bg_visual = "#275378"
-    --         local fg = "#CBE0F0"
-    --         local fg_dark = "#B4D0E9"
-    --         local fg_gutter = "#627E97"
-    --         local border = "#547998"
-    --
-    --         require("tokyonight").setup({
-    --             on_highlights = function(hl, c)
-    --                 hl.CursorLineNr = { fg = c.orange, bold = true }
-    --                 hl.DiagnosticHint = { fg = c.blue }
-    --                 hl.DiagnosticHintUnderline = { fg = c.blue, undercurl = true }
-    --             end,
-    --             style = "night",
-    --             transparent = transparent,
-    --             styles = {
-    --                 sidebars = transparent and "transparent" or "dark",
-    --                 floats = transparent and "transparent" or "dark",
-    --             },
-    --             on_colors = function(colors)
-    --                 colors.bg = bg
-    --                 colors.bg_dark = transparent and colors.none or bg_dark
-    --                 colors.bg_float = transparent and colors.none or bg_dark
-    --                 colors.bg_highlight = bg_highlight
-    --                 colors.bg_popup = bg_dark
-    --                 colors.bg_search = bg_search
-    --                 colors.bg_sidebar = transparent and colors.none or bg_dark
-    --                 colors.bg_statusline = transparent and colors.none or bg_dark
-    --                 colors.bg_visual = bg_visual
-    --                 colors.border = border
-    --                 colors.fg = fg
-    --                 colors.fg_dark = fg_dark
-    --                 colors.fg_float = fg
-    --                 colors.fg_gutter = fg_gutter
-    --                 colors.fg_sidebar = fg_dark
-    --             end,
-    --         })
-    --
-    --         vim.cmd("colorscheme tokyonight")
+    --         vim.cmd.colorscheme ""
+    --         -- ColorMyPencils()
     --     end,
     -- }
 }
--- zsh/.zprofile
+++ zsh/.zprofile
@@ -1,6 +1,9 @@
 typeset -U path
 # path
 path=("$HOME/.local/bin"
+    "${AQUA_ROOT_DIR}/bin"
+    "/opt/homebrew/bin"
+    "$NPM_CONFIG_PREFIX/bin"
     "$HOME/go/bin"
     "$DENO_INSTALL_ROOT/bin"
     "$DENO_INSTALL_ROOT/env"
@@ -15,13 +18,11 @@ path=("$HOME/.local/bin"
     /sbin
     /usr/local/bin
     /usr/local/cuda/bin
-    /opt/homebrew/opt/asdf/libexec/asdf.sh
-    "$HOME/.asdf/shims"
     "$GOPATH/bin"
+    "/opt/local/bin:/opt/local/sbin"
 )
 export PATH
 
 # MacPorts Installer addition on 2023-03-01_at_13:12:17: adding an appropriate PATH variable for use with MacPorts.
-# export PATH="/opt/local/bin:/opt/local/sbin:$PATH"
 # Finished adapting your PATH environment variable for use with MacPorts.
 eval "$(/opt/homebrew/bin/brew shellenv)"
