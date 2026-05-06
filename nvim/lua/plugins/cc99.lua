145b8d9e91b6ea3ef5c96c8d8ba0e1cd6d96c1f1
Taichi Araki <araki130710@gmail.com>
2026-05-07 02:17:38 +0900

    chore(nvim): enable cc99 and disable copilot

--- nvim/lua/plugins/cc99.lua
+++ nvim/lua/plugins/cc99.lua
@@ -1,8 +1,6 @@
--- return {
--- 	dir = "~/personalDev/cc99",
--- 	config = function()
--- 		require("cc99")
--- 	end,
--- }
---
-return {}
+return {
+	dir = "~/personalDev/cc99",
+	config = function()
+		require("cc99")
+	end,
+}
