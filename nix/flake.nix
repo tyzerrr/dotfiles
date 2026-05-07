e5ea30e34fc7d460b6f0ea0af481a1b1f71e51ac
Taichi Araki <araki130710@gmail.com>
2026-05-08 00:00:57 +0900

    chore(nix): add some packages

--- nix/flake.nix
+++ nix/flake.nix
@@ -7,12 +7,20 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-claude-code = {
+      url = "github:ryoppippi/nix-claude-code";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: 
+  outputs = { self, nixpkgs, home-manager, nix-claude-code, ... }:
     let
       system = "aarch64-darwin";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+        overlays = [ nix-claude-code.overlays.default ];
+      };
     in {
       homeConfigurations.araki = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
--- nix/home.nix
+++ nix/home.nix
@@ -5,5 +5,43 @@
   home.stateVersion = "25.11";
   home.packages = with pkgs; [
     nodejs_24
+    tree-sitter
+    pnpm
+    terraform
+    ripgrep
+    go_1_26
+    fd
+    jq
+    fzf
+    tmux
+    ghq
+    bat
+    eza
+    zoxide
+    vim
+    neovim
+    awscli2
+    gh
+    golangci-lint
+    kubernetes-helm
+    minikube
+    kustomize
+    lazygit
+    lsd
+    kind
+    docker-client
+    tflint
+    kubectx
+    uv
+    ruff
+    ssm-session-manager-plugin
+    stylua
+    deno
+    google-cloud-sdk
+    python313
+    claude-code
+    rustc
+    cargo
+    yq-go
   ];
 }
