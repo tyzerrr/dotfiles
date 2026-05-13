0180184becf2897e09014be36e5c1542bdb986cd
Taichi Araki <t-b-araki@mercari.com>
2026-05-13 22:28:26 +0900

    chore(nvim-treesitter): add prisma

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,30 +1,47 @@
 -- lua/plugins/treesitter.lua
 local parsers = {
-  "bash", "c", "cpp", "css", "go", "html", "java", "javascript",
-  "json", "lua", "markdown", "markdown_inline", "python",
-  "query", "rust", "typescript", "vim", "vimdoc", "yaml",
+	"bash",
+	"c",
+	"cpp",
+	"css",
+	"go",
+	"html",
+	"java",
+	"javascript",
+	"json",
+	"lua",
+	"markdown",
+	"markdown_inline",
+	"python",
+	"query",
+	"rust",
+	"typescript",
+	"vim",
+	"vimdoc",
+	"yaml",
+	"prisma",
 }
 
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    branch = "main",
-    lazy = false,
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter").setup({
-        install_dir = vim.fn.stdpath("data") .. "/site",
-      })
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		lazy = false,
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter").setup({
+				install_dir = vim.fn.stdpath("data") .. "/site",
+			})
 
-      require("nvim-treesitter").install(parsers)
+			require("nvim-treesitter").install(parsers)
 
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = parsers,
-        callback = function()
-          pcall(vim.treesitter.start)
-          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-        end,
-      })
-    end,
-  },
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = parsers,
+				callback = function()
+					pcall(vim.treesitter.start)
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
+			})
+		end,
+	},
 }
