411c4943f194670f112c06f7ca10518f422045bc
Taichi Araki <t-b-araki@mercari.com>
2026-05-14 20:43:30 +0900

    chore(nvim): update treesitter setting

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -1,24 +1,23 @@
 return {
-    "ibhagwan/fzf-lua",
-    -- optional for icon support
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    -- or if using mini.icons/mini.nvim
-    -- dependencies = { "nvim-mini/mini.icons" },
-    ---@module "fzf-lua"
-    ---@type fzf-lua.Config|{}
-    ---@diagnostics disable: missing-fields
-    keys = {
-        { "<leader>pf",  "<cmd>FzfLua files<cr>",               desc = "Find files" },
-        { "<leader>ps",  "<cmd>FzfLua live_grep<cr>",           desc = "Search in files" },
-        { "<leader>gf",  "<cmd>FzfLua git_files<cr>",           desc = "Git files" },
-        { "<leader>gd",  "<cmd>FzfLua git_diff<cr>",            desc = "Git diff" },
-        { "<leader>gs",  "<cmd>FzfLua git_status<cr>",          desc = "Git status" },
-        { "<leader>gb",  "<cmd>FzfLua git_branches<cr>",        desc = "Git branches" },
-        { "gr",          "<cmd>FzfLua lsp_references<cr>",      desc = "LSP references" },
-        { "gd",          "<cmd>FzfLua lsp_definitions<cr>",     desc = "LSP definitions" },
-        { "gI",          "<cmd>FzfLua lsp_implementations<cr>", desc = "LSP implementations" },
-        { "<leader>lca", "<cmd>FzfLua lsp_code_actions<cr>",    desc = "LSP code actions" },
-    },
-    opts = {
-    }
+	"ibhagwan/fzf-lua",
+	-- optional for icon support
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	-- or if using mini.icons/mini.nvim
+	-- dependencies = { "nvim-mini/mini.icons" },
+	---@module "fzf-lua"
+	---@type fzf-lua.Config|{}
+	---@diagnostics disable: missing-fields
+	keys = {
+		{ "<leader>pf", "<cmd>FzfLua files<cr>", desc = "Find files" },
+		{ "<leader>ps", "<cmd>FzfLua live_grep<cr>", desc = "Search in files" },
+		{ "<leader>gf", "<cmd>FzfLua git_files<cr>", desc = "Git files" },
+		{ "<leader>gd", "<cmd>FzfLua git_diff<cr>", desc = "Git diff" },
+		{ "<leader>gs", "<cmd>FzfLua git_status<cr>", desc = "Git status" },
+		{ "<leader>gb", "<cmd>FzfLua git_branches<cr>", desc = "Git branches" },
+		{ "gr", "<cmd>FzfLua lsp_references<cr>", desc = "LSP references" },
+		{ "gd", "<cmd>FzfLua lsp_definitions<cr>", desc = "LSP definitions" },
+		{ "gI", "<cmd>FzfLua lsp_implementations<cr>", desc = "LSP implementations" },
+		{ "<leader>lca", "<cmd>FzfLua lsp_code_actions<cr>", desc = "LSP code actions" },
+	},
+	opts = {},
 }
