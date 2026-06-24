0a9078a9ae82c52eb70e2e4bb2add87ed311ca8b
Taichi Araki <t-b-araki@mercari.com>
2026-06-24 17:06:35 +0900

    chore(wezterm): udpate color theme

--- wezterm/wezColorScheme.lua
+++ wezterm/wezColorScheme.lua
@@ -1,11 +1,12 @@
 local module = {}
 
 function module.apply_to_config(config)
-    -- config.color_scheme = "Batman"
-    config.color_scheme = "tokyonight"
-    -- config.color_scheme = "Tomorrow Night"
-    -- config.color_scheme = "Monokai (dark) (terminal.sexy)"
-    -- config.color_scheme = "Solarized Dark"
+	-- config.color_scheme = "Batman"
+	-- config.color_scheme = "tokyonight"
+	config.color_scheme = "Kanagawa Dragon (Gogh)"
+	-- config.color_scheme = "Tomorrow Night"
+	-- config.color_scheme = "Monokai (dark) (terminal.sexy)"
+	-- config.color_scheme = "Solarized Dark"
 end
 
 return module
