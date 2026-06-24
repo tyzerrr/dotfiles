31ecd6f48221a413bcc9c3c168adab730cea7457
Taichi Araki <t-b-araki@mercari.com>
2026-06-24 17:06:49 +0900

    chore(wezterm): enable auto reload config

--- wezterm/wezInit.lua
+++ wezterm/wezInit.lua
@@ -3,6 +3,7 @@ local module = {}
 function module.apply_to_config(config)
 	config.scrollback_lines = 3500
 	config.enable_scroll_bar = true
+	config.automatically_reload_config = true
 	config.macos_window_background_blur = 20
 	config.window_decorations = "RESIZE"
 end
