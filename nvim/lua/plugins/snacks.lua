3f144bf36fbec611778a67a770826114ee8f480f
Taichi Araki <t-b-araki@mercari.com>
2026-07-01 13:00:41 +0900

    chore(nvim): add snacks.gh keybind

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,1 +1,33 @@
-return {}
+return {
+	"folke/snacks.nvim",
+	keys = {
+		{
+			"<leader>gi",
+			function()
+				Snacks.picker.gh_issue()
+			end,
+			desc = "Github Issue (Open)",
+		},
+		{
+			"<leader>gI",
+			function()
+				Snacks.picker.gh_issue({ state = "all" })
+			end,
+			desc = "Github Issue (All)",
+		},
+		{
+			"<leader>gp",
+			function()
+				Snacks.picker.gh_pr()
+			end,
+			desc = "Github PR (Open)",
+		},
+		{
+			"<leader>gP",
+			function()
+				Snacks.picker.gh_pr({ state = "all" })
+			end,
+			desc = "Github PR (All)",
+		},
+	},
+}
