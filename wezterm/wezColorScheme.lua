52528cc31128214181cabe72fde544c0a1db2f82
Taichi Araki <t-b-araki@mercari.com>
2026-07-16 17:54:20 +0900

    chore(wezterm): update colorschema to RosePine

--- wezterm/wezColorScheme.lua
+++ wezterm/wezColorScheme.lua
@@ -2,11 +2,12 @@ local module = {}
 
 function module.apply_to_config(config)
 	-- config.color_scheme = "Batman"
-	config.color_scheme = "tokyonight"
+	-- config.color_scheme = "tokyonight"
 	-- config.color_scheme = "Kanagawa Dragon (Gogh)"
 	-- config.color_scheme = "Tomorrow Night"
 	-- config.color_scheme = "Monokai (dark) (terminal.sexy)"
 	-- config.color_scheme = "Solarized Dark"
+	config.color_scheme = "Rosé Pine (Gogh)"
 end
 
 return module
